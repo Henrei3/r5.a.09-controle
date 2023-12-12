8f8802711d3d04e319af614288b574fa7f31775e
Henrei3 <95311577+Henrei3@users.noreply.github.com>
2023-12-12 12:31:42 +0100

    matomo

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker compose -f loki.yml -f traefik.yml -f ./wordpress/wordpress.yml -f ./matomo/matomo.yml up -d
+docker compose -f loki.yml -f traefik.yml -f ./wordpress/wordpress.yml -f ./matomo/matomo.yml -f ./keycloak/keycloak.yml ./magento/magento.yml up -d
